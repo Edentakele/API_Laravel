2daaa03f2aa7353146d97f9f72ac9d8d5f8eb6ce
eden takele <se.eden.takele@gmail.com>
2024-04-29 11:54:08 +0300

    api first test is done

--- database/migrations/2024_04_29_072258_create_tasks_table.php
+++ database/migrations/2024_04_29_072258_create_tasks_table.php
@@ -15,8 +15,10 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->boolean('completed')->default(false);
             $table->timestamps();
-
         });
     }
 
